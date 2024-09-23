6cf868236ba4b33d1277393b30bc630610d638bc
RomulusMirauta <romulus.mirauta1@gmail.com>
2024-09-23 22:26:59 +0300

    emptied repo

--- sesiunea_12/sesiunea12_studiu_echipa.py
+++
@@ -1,9 +0,0 @@
-"""
-EXERCITII EXERCITII STUDIU IN ECHIPA (Sesiunea 12)
-"""
-
-"""
-Continuand implementarea bazei de date pentru aplicatia marketplace
-inceputa in sesiunea 12 live, executa toate operatiile CRUD (prin query-uri SQL)
-pe tabelul users.
-"""
